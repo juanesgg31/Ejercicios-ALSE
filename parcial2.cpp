9e75e501590ed5723741a887191cfb094f726c50
Juan Esteban Espinosa Acevedo <jespinosa_acevedo@outlook.com>
2025-09-18 22:53:39 -0500

    Haciendo los arreglos correspondientes.

--- parcial2.cpp
+++ parcial2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 struct Point
 {
@@ -8,27 +9,27 @@ struct Point
 
 double calcularMagnitud(Point p1)
 {
-
     return std::sqrt(p1.x * p1.x + p1.y * p1.y);
 }
 
 void leerPuntos(Point puntos[], int n)
 {
     char respuesta;
-    std::cout << "¿Desea ingresar los puntos manualmente? (s/n): ";
-    std::cin >> respuesta;
+    cout << "¿Desea ingresar los puntos manualmente? (s/n): ";
+    cin >> respuesta;
 
-    if (respuesta == 's' || respuesta == 'S')
+    if (respuesta == 's')
     {
         for (int i = 0; i < n; ++i)
         {
-            std::cout << "Ingrese las coordenadas del punto " << i + 1 << " (x, y): ";
-            std::cin >> puntos[i].x >> puntos[i].y;
+            cout << "Ingrese las coordenadas del punto " << i + 1 << " (x, y): ";
+            char lp, comma, rp;
+            cin >> lp >> puntos[i].x >> comma >> puntos[i].y >> rp;
         }
     }
     else
     {
-        std::cout << "Usando puntos predeterminados...\n";
+        cout << "Usando puntos predeterminados...\n";
         Point pred[] = {{0, 0}, {3, 4}, {6, 8}, {9, 12}};
         for (int i = 0; i < n; ++i)
             puntos[i] = (i < 4) ? pred[i] : Point{0, 0};
@@ -39,7 +40,6 @@ double calcularMayorMagnitud(Point puntos[], int n, int &indiceMayorMagnitud)
 {
     indiceMayorMagnitud = 0;
     double mayorMagnitud = calcularMagnitud(puntos[0]);
-
     for (int i = 1; i < n; ++i)
     {
         double magnitud = calcularMagnitud(puntos[i]);
@@ -54,33 +54,26 @@ double calcularMayorMagnitud(Point puntos[], int n, int &indiceMayorMagnitud)
 
 void mostrarResultado(Point puntos[], int indiceMayorMagnitud, double mayorMagnitud)
 {
-    std::cout << "El punto de mayor magnitud es: ("
-              << puntos[indiceMayorMagnitud].x << ", "
-              << puntos[indiceMayorMagnitud].y << ")\n";
-    std::cout << "La mayor magnitud del arreglo es: " << mayorMagnitud << std::endl;
+    cout << "El punto de mayor magnitud es: ("
+         << puntos[indiceMayorMagnitud].x << ", "
+         << puntos[indiceMayorMagnitud].y << ")\n";
+    cout << "La mayor magnitud del arreglo es: " << mayorMagnitud << endl;
 }
 
 int main()
 {
     int n;
-
-    std::cout << "Ingrese el número de puntos (mínimo 2): ";
-    std::cin >> n;
-
+    cout << "Ingrese el número de puntos (mínimo 2): ";
+    cin >> n;
     if (n < 2)
     {
-        std::cout << "Se necesitan al menos 2 puntos para determinar la mayor magnitud.\n";
+        cout << "Se necesitan al menos 2 puntos para determinar la mayor magnitud.\n";
         return 1;
     }
-
     Point puntos[n];
-
     leerPuntos(puntos, n);
-
     int indiceMayorMagnitud;
     double mayorMagnitud = calcularMayorMagnitud(puntos, n, indiceMayorMagnitud);
-
     mostrarResultado(puntos, indiceMayorMagnitud, mayorMagnitud);
-
     return 0;
 }
